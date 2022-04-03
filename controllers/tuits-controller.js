ae614f0b1e10222fd0628097a558aeaa6750dfb5
klaida <66129477+klaidaazizi@users.noreply.github.com>
2022-04-03 11:04:15 -0400

    a8 REST sp22

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -1,19 +1,28 @@
 import posts from './data/tuits.js';
 let tuits = posts;
 
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findAllTuits);
+    app.put('/api/tuits/:tid',updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
+
 const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = (new Date()).getTime()+'';
     newTuit.likes = 0;
+    newTuit.dislikes = 0;
     tuits.push(newTuit);
     res.json(newTuit);
 }
 const findAllTuits = (req,res) => {
     res.json(tuits);
+    res.sendStatus(200);
 }
 
 const updateTuit = (req, res) => {
-    const tuitdIdToUpdate = req.params.tid;
+    const tuitdIdToUpdate = req.params.id;
     const updatedTuit = req.body;
     tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
     res.sendStatus(200);
@@ -24,12 +33,4 @@ const deleteTuit = (req, res) => {
     const tuitdIdToDelete = req.params.tid;
     tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
     res.sendStatus(200);
-}
-
-
-export default (app) => {
-    app.post('/api/tuits', createTuit);
-    app.get('/api/tuits', findAllTuits);
-    app.put('/api/tuits/:tid',updateTuit);
-    app.delete('/api/tuits/:tid', deleteTuit);
 }
