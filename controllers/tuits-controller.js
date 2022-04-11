1823aa0388947c4a3d9539dae0a7e468078f7524
klaida <66129477+klaidaazizi@users.noreply.github.com>
2022-04-11 13:42:51 -0400

    a9 Mongo sp22

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -1,5 +1,4 @@
-import posts from './data/tuits.js';
-let tuits = posts;
+import * as tuitsDao from '../database/tuits-dao.js';
 
 export default (app) => {
     app.post('/api/tuits', createTuit);
@@ -8,28 +7,26 @@ export default (app) => {
     app.delete('/api/tuits/:tid', deleteTuit);
 }
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
     const newTuit = req.body;
-    newTuit._id = (new Date()).getTime()+'';
-    newTuit.likes = 0;
-    newTuit.dislikes = 0;
-    tuits.push(newTuit);
-    res.json(newTuit);
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    res.json(insertedTuit);
 }
-const findAllTuits = (req,res) => {
+const findAllTuits = async (req,res) => {
+    const tuits = await tuitsDao.findAllTuits()
     res.json(tuits);
 }
 
-const updateTuit = (req, res) => {
-    const tuitdIdToUpdate = req.params.id;
+const updateTuit = async (req, res) => {
+    const tuitIdToUpdate = req.params.id;
     const updatedTuit = req.body;
-    tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
-    res.sendStatus(200);
+    const status = await tuitsDao.updateTuit(tuitIdToUpdate, updatedTuit);
+    res.send(status);
 }
 
 
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
     const tuitdIdToDelete = req.params.tid;
-    tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
-    res.sendStatus(200);
+    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+    res.send(status);
 }
--- server.js
+++ server.js
@@ -3,12 +3,15 @@ import cors from 'cors';
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
+import mongoose from "mongoose";
+const DB_CONNECTION_STRING = 'mongodb+srv://klaidaazizi:p4ssw0rd@cluster0.1ooqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(DB_CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-helloController(app);
-userController(app);
+// helloController(app);
+// userController(app);
 tuitsController(app);
-app.listen(process.env.PORT || 4000);
+app.listen(4000);
